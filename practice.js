dff4408aba9e940e597e5caafdfa91574ce2f5e2
Md. Anwar Hossain <aymankajolanwar@gmail.com>
2025-09-12 21:12:09 +0600

    this is an another update of js file

---
+++ practice.js
@@ -0,0 +1,56 @@
+/**
+ var test = "this is a string type data";
+
+var num = 20;
+
+var boolian = true;
+
+console.log(typeof test);
+
+console.log(typeof boolian);
+
+
+var stringToNumber = "20";
+
+var num = Number(stringToNumber);s
+
+var result = num + 20;
+
+
+console.log(typeof result);
+
+var string = "this is a string";
+
+console.log(string);
+console.log(typeof string);
+
+
+var num = 20;
+
+console.log(num);
+console.log(typeof num);
+
+var stringNumber = Number('20');
+
+var result = num + stringNumber;
+
+console.log (result);
+console.log (typeof result);
+ */
+
+
+// var applePrice = parseInt ("20.45");
+// var binjalPrice = 30;
+
+// var result = (applePrice + binjalPrice)*2;
+
+
+// console.log(result);
+
+
+// var applePrice = parseFloat(20.547883);
+// var tormujPrice = 20;
+
+// var result = applePrice + tormujPrice;
+
+// console.log(result.toFixed(1));
